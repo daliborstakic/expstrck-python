baf29992c36376a88e40bd87fafeda50239bc7c2
daliborstakic <stakicdalibor64@gmail.com>
2020-09-03 16:28:59 +0200

    added third row with listbox

--- gui_expense.py
+++ gui_expense.py
@@ -12,12 +12,17 @@ name_field = tk.Entry(root)
 cost_label = tk.Label(root, text="Cost:")
 cost_field = tk.Entry(root)
 
+# Third row
+expense_list = tk.Listbox(root)
+
 # Placing elements on the grid
 name_label.grid(row=0, column=0, padx=5, pady=5)
 name_field.grid(row=0, column=1, padx=5, pady=5)
 
 cost_label.grid(row=1, column=0, padx=5, pady=5)
 cost_field.grid(row=1, column=1, padx=5, pady=5)
+
+expense_list.grid(row=2, columnspan=2, padx=5, pady=(5, 10))
 
 # Tkinter mainloop
 tk.mainloop()
