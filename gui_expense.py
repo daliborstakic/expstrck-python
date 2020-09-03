f6be5ff3923b48afde03e4413b50d56c35f203b8
daliborstakic <stakicdalibor64@gmail.com>
2020-09-03 16:02:11 +0200

    added first row of elements

--- gui_expense.py
+++ gui_expense.py
@@ -4,5 +4,13 @@ import tkinter as tk
 # Main window
 root = tk.Tk()
 
+# First row
+name_label = tk.Label(root, text="Name:")
+name_field = tk.Entry(root)
+
+# Placing elements on the grid
+name_label.grid(row=0, column=0)
+name_field.grid(row=0, column=1)
+
 # Tkinter mainloop
 tk.mainloop()
