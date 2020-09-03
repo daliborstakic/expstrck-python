11c365eb29481548858e5abe7bad38be1f0747da
daliborstakic <stakicdalibor64@gmail.com>
2020-09-03 16:07:42 +0200

    added padding

--- gui_expense.py
+++ gui_expense.py
@@ -8,9 +8,16 @@ root = tk.Tk()
 name_label = tk.Label(root, text="Name:")
 name_field = tk.Entry(root)
 
+# Second row
+cost_label = tk.Label(root, text="Cost:")
+cost_field = tk.Entry(root)
+
 # Placing elements on the grid
-name_label.grid(row=0, column=0)
-name_field.grid(row=0, column=1)
+name_label.grid(row=0, column=0, padx=5, pady=5)
+name_field.grid(row=0, column=1, padx=5, pady=5)
+
+cost_label.grid(row=1, column=0, padx=5, pady=5)
+cost_field.grid(row=1, column=1, padx=5, pady=5)
 
 # Tkinter mainloop
 tk.mainloop()
