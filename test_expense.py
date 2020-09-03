714815b7e9d70260b312c857387ee9c4181a2b07
daliborstakic <stakicdalibor64@gmail.com>
2020-09-03 10:37:33 +0200

    added test_cost

--- test_expense.py
+++ test_expense.py
@@ -24,6 +24,27 @@ class ExpenseTest(unittest.TestCase):
         self.assertEqual(self.exp1.name, "Chocolate")
         self.assertEqual(self.exp2.name, "Carrot")
 
+    def test_cost(self):
+        """ Testing cost attribute functionality """
+
+        # If the cost is negative
+        with self.assertRaises(ValueError):
+            exp3 = Expense("Bag", -2)
+
+        # Testing getter and setter
+        with self.assertRaises(ValueError):
+            self.exp1.cost = -2 # Cost cannot be negative
+
+        self.assertEqual(self.exp1.cost, 2.5)
+        self.assertEqual(self.exp2.cost, 3.5)
+
+        self.exp1.cost = 10
+        self.exp2.cost = 5
+
+        self.assertEqual(self.exp1.cost, 10)
+        self.assertEqual(self.exp2.cost, 5)
+
+
 if __name__ == '__main__':
     """ Just so I can run it without python in the console """
     unittest.main()
