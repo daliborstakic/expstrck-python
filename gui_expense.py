de384b93bbc22a141c7d4adc2fff5bf6e9f09841
daliborstakic <stakicdalibor64@gmail.com>
2020-09-04 08:45:06 +0200

    added two buttons in row 4

--- gui_expense.py
+++ gui_expense.py
@@ -15,6 +15,10 @@ cost_field = tk.Entry(root)
 # Third row
 expense_list = tk.Listbox(root)
 
+# Fourth row
+button_add = tk.Button(root, text="Add")
+button_remove = tk.Button(root, text="Remove")
+
 # Placing elements on the grid
 name_label.grid(row=0, column=0, padx=5, pady=5)
 name_field.grid(row=0, column=1, padx=5, pady=5)
@@ -22,7 +26,10 @@ name_field.grid(row=0, column=1, padx=5, pady=5)
 cost_label.grid(row=1, column=0, padx=5, pady=5)
 cost_field.grid(row=1, column=1, padx=5, pady=5)
 
-expense_list.grid(row=2, columnspan=2, padx=5, pady=(5, 10))
+expense_list.grid(row=2, columnspan=2, padx=5, pady=5)
+
+button_add.grid(row=3, column=0, padx=5, pady=5)
+button_remove.grid(row=3, column=1, padx=5, pady=5)
 
 # Tkinter mainloop
 tk.mainloop()
