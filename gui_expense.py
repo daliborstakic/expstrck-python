6d48f3ecfdd8669ac3dab4f799970439b209e5ef
daliborstakic <stakicdalibor64@gmail.com>
2020-09-04 10:06:15 +0200

    added functionality to the add button program now writes and reads from a .csv file

--- gui_expense.py
+++ gui_expense.py
@@ -1,5 +1,8 @@
 """ Importing Tkinter """
 import tkinter as tk
+from tkinter import messagebox
+import csv
+from expense import Expense
 
 # Main window
 root = tk.Tk()
@@ -15,8 +18,60 @@ cost_field = tk.Entry(root)
 # Third row
 expense_list = tk.Listbox(root)
 
+def add_expense():
+    """ Adding expenses to the .csv file and the ListBox """
+    if name_field.get() == "" or cost_field.get() == "":
+        error_message("You haven't entered any data!")
+
+    try: # If the user tries to enter something else than a number
+        cost = int(cost_field.get())
+        name = name_field.get() # Storing the name value
+        temp_exp = Expense(name, cost)
+    except ValueError:
+        cost_field.delete(0, tk.END) # Clearing the entry fields
+        name_field.delete(0, tk.END)
+        temp_exp = None
+        error_message("You need to enter a number!")
+
+    if temp_exp:
+        """ If temp_exp is not None """
+        with open("data.csv", "a", newline='') as csv_file:
+            """ Writing to the .csv file """
+            dict_writer = csv.DictWriter(csv_file, fieldnames=['name', 'cost'])
+            dict_writer.writerow({'name': temp_exp.name, 'cost': temp_exp.cost})
+            expense_list.insert(tk.END, f"{temp_exp.name} - {temp_exp.cost}")
+
+        cost_field.delete(0, tk.END) # Clearing the entry fields
+        name_field.delete(0, tk.END)
+        exp.append(temp_exp) # Adding expense to expense list
+        total_cost(exp) # Updating the total cost
+
+def csv_to_list(exp):
+    """ Fills the list on startup """
+    with open("data.csv") as csv_file:
+        reader = csv.DictReader(csv_file)
+
+        for row in reader:
+            expense_list.insert(tk.END, f"{row['name']} - {row['cost']}")
+            exp.append(Expense(row['name'], int(row['cost'])))
+
+def error_message(_message):
+    """ Displays message """
+    messagebox.showerror("Error", _message)
+
+def total_cost(exp):
+    """ Calculates the total cost and displays it """
+    total_cost = sum(obj.cost for obj in exp)
+    total_label = tk.Label(root, text=f"Total cost is: {total_cost}")
+    total_label.grid(row=4, column=0, padx=5, pady=5)
+
+# Expense list
+exp = []
+csv_to_list(exp)
+total_cost(exp)
+
 # Fourth row
-button_add = tk.Button(root, text="Add")
+button_add = tk.Button(root, text="Add", command=add_expense)
 button_remove = tk.Button(root, text="Remove")
 
 # Placing elements on the grid
